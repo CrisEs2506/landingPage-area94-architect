3767c3a7a606d5096a919d41d74131cca67e8982
CrisEs2506 <cristianespitia2506@gmail.com>
2026-03-24 11:03:58 -0500

    Se añade adaptador

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  adapter: node({ mode: 'standalone' }),
   integrations: [icon()],
 });
