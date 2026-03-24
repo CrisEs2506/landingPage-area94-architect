315557d3dd7087ece020802674c28e853aa8af59
CrisEs2506 <cristianespitia2506@gmail.com>
2026-03-24 10:54:56 -0500

    Configuración para pasar de SSR a Estatico

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'static',
   integrations: [icon()],
 });
